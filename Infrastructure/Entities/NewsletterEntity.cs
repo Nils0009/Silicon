414618e98574816d49cd2b709e0ea26aa0dede53
Nils <Nils0009@hotmail.com>
2024-03-09 10:12:41 +0100

    updated auth

---
+++ Infrastructure/Entities/NewsletterEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Entities;
+
+public class NewsletterEntity
+{
+    public int Id { get; set; }
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekInReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+
+    public ICollection<UserEntity> Users { get; set; } = [];
+}
