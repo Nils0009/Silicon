f9743693cdff0364d8df7ffeb73bd316964cd99c
Nils <Nils0009@hotmail.com>
2024-03-11 09:06:58 +0100

    updated data base and added bio in user tabel. Account controller with views and models are under construction

--- Infrastructure/Entities/AddressEntity.cs
+++ Infrastructure/Entities/AddressEntity.cs
@@ -4,6 +4,7 @@ public class AddressEntity
 {
     public int Id { get; set; }
     public string StreetName { get; set; } = null!;
+    public string? AddressLine2 { get; set; }
     public string City { get; set; } = null!;
     public string PostalCode { get; set; } = null!;
 
